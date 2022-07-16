a5ecfb99339fd5c7bc3b531005a711a9688dfbc7
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-07-16 14:04:44 -0400

    css and javascript
    created the function to change questions and eventually bring you to the end of the game

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 var question = document.getElementById("question");
-var choices =Array.from (document.getElementsByClassName("choice-text"));
+var choices = Array.from(document.getElementsByClassName("choice-text"));
 
 var currentQuestion = {};
 var acceptingAnswers = true;
@@ -7,53 +7,53 @@ var score = 0;
 var questionCounter = 0;
 var availableQuestions = [];
 
-var questions = [ 
-    
+var questions = [
 
- {    
-     question: "Inside Which HTML Element do we put the Javascript",
-     choice1: "<script>",
-     choice2: "<javascript>",
-     choice3: "<js>",
-     choice4: "<scripting>",
-     answer: 1
- },
- {
 
-     question: "Where is the correct place to insert a Javascript", 
-     choice1:"Both the <head and the <body> section are correct",
-     choice2:"the <body> section",
-     choice3:"The <head> section",
-     choice4:"none of the above",
-     answer: 1
- },
- {
-     question: "How can you add a comment in Javascript",
-     choice1: "'This is a comment",
-     choice2:"<!--This is a comment-->",
-     choice3:"//this is a comment",
-     choice4:"none of the above",
-     answer: 3
+    {
+        question: "Inside Which HTML Element do we put the Javascript",
+        choice1: "<script>",
+        choice2: "<javascript>",
+        choice3: "<js>",
+        choice4: "<scripting>",
+        answer: 1
+    },
+    {
 
- },
- {
-     question: "What is the correct way to write a Javascript array",
-     choice1: "var colors=(1:'red', 2:'green', 3:'blue') ",
-     choice2: "var colors=['red','green','blue',]",
-     choice3: "var colors='red','green','blue'",
-     choice4: "var=1=('red'),('green'),('blue')",
-     answer: 2
+        question: "Where is the correct place to insert a Javascript",
+        choice1: "Both the <head and the <body> section are correct",
+        choice2: "the <body> section",
+        choice3: "The <head> section",
+        choice4: "none of the above",
+        answer: 1
+    },
+    {
+        question: "How can you add a comment in Javascript",
+        choice1: "'This is a comment",
+        choice2: "<!--This is a comment-->",
+        choice3: "//this is a comment",
+        choice4: "none of the above",
+        answer: 3
 
- },
- {
-     question: "which event occurs when the user clicks on an HTML element",
-     choice1: "onMouseClick",
-     choice2: "onClick",
-     choice3: "onMouseOver",
-     choice4: "onChange",
-     answer: 2
-      
- },
+    },
+    {
+        question: "What is the correct way to write a Javascript array",
+        choice1: "var colors=(1:'red', 2:'green', 3:'blue') ",
+        choice2: "var colors=['red','green','blue',]",
+        choice3: "var colors='red','green','blue'",
+        choice4: "var=1=('red'),('green'),('blue')",
+        answer: 2
+
+    },
+    {
+        question: "which event occurs when the user clicks on an HTML element",
+        choice1: "onMouseClick",
+        choice2: "onClick",
+        choice3: "onMouseOver",
+        choice4: "onChange",
+        answer: 2
+
+    },
 ];
 
 
@@ -65,5 +65,38 @@ startGame = () => {
     score = 0;
     availableQuestions = [...questions]
     console.log(availableQuestions);
-}
+    getNewQuestion();
+};
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        return window.location.assign('/end.html');
+    }
+
+    questionCounter++;
+    var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+
+    choices.forEach(choice => {
+        var number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
+    availableQuestions.splice(questionIndex, 1);
+    console.log(availableQuestions);
+    acceptingAnswers = true;
+};
+choices.forEach(choice => {
+    choice.addEventListener(click, e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        var selectedChoice = e.target;
+        var selectedAnswer = selectedChoice.dataset["number"];
+
+        getNewQuestion();
+
+    });
+});
+
 startGame();
