4f38365b03bbafc03d2a4215f1d7d60bd868f3ac
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-07-16 13:31:26 -0400

    created question objects  and started on my start game function
    working on the Javascript portion. for the numbering of the questions. i did not use 0 indexing i put the numbers in the choices so that they match up that way

--- js/game.js
+++ js/game.js
@@ -7,49 +7,63 @@ var score = 0;
 var questionCounter = 0;
 var availableQuestions = [];
 
-var questions =[ 
-{
+var questions = [ 
     
-    question: "Inside Which HTML Element do we put the Javascript",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1
-},
-{
 
-    question: "Where is the correct place to insert a Javascript", 
-    choice1:"Both the <head and the <body> section are correct",
-    choice2:"the <body> section",
-    choice3:"The <head> section",
-    choice4:"none of the above",
-    answer: 1
-},
-{
-    question: "How can you add a comment in Javascript",
-    choice1: "'This is a comment",
-    choice2:"<!--This is a comment-->",
-    choice3:"//this is a comment",
-    choice4:"none of the above",
-    answer: 3
+ {    
+     question: "Inside Which HTML Element do we put the Javascript",
+     choice1: "<script>",
+     choice2: "<javascript>",
+     choice3: "<js>",
+     choice4: "<scripting>",
+     answer: 1
+ },
+ {
 
-},
-{
-    question: "What is the correct way to write a Javascript array",
-    choice1: "var colors=(1:'red', 2:'green', 3:'blue') ",
-    choice2: "var colors=['red','green','blue',]",
-    choice3: "var colors='red','green','blue'",
-    choice4: "var=1=('red'),('green'),('blue')"
+     question: "Where is the correct place to insert a Javascript", 
+     choice1:"Both the <head and the <body> section are correct",
+     choice2:"the <body> section",
+     choice3:"The <head> section",
+     choice4:"none of the above",
+     answer: 1
+ },
+ {
+     question: "How can you add a comment in Javascript",
+     choice1: "'This is a comment",
+     choice2:"<!--This is a comment-->",
+     choice3:"//this is a comment",
+     choice4:"none of the above",
+     answer: 3
 
-},
-{
-    question: "which event occurs when the user clicks on an HTML element",
-    choice1: "onMouseClick",
-    choice2: "onClick",
-    choice3: "onMouseOver",
-    choice4: "onChange",
+ },
+ {
+     question: "What is the correct way to write a Javascript array",
+     choice1: "var colors=(1:'red', 2:'green', 3:'blue') ",
+     choice2: "var colors=['red','green','blue',]",
+     choice3: "var colors='red','green','blue'",
+     choice4: "var=1=('red'),('green'),('blue')",
+     answer: 2
 
+ },
+ {
+     question: "which event occurs when the user clicks on an HTML element",
+     choice1: "onMouseClick",
+     choice2: "onClick",
+     choice3: "onMouseOver",
+     choice4: "onChange",
+     answer: 2
       
-},
-]
+ },
+];
+
+
+var CORRECT_BONUS = 10;
+var MAX_QUESTIONS = 5;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions]
+    console.log(availableQuestions);
+}
+startGame();
